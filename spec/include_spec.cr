c3ac26c904a615f3cb8a9725cc8e925af4822af7
tkshnwesper <tkshnwesper@users.noreply.github.com>
2022-04-10 18:26:25 +0530

    add ability to include methods,consts,class,subclass

--- spec/include_spec.cr
+++ spec/include_spec.cr
@@ -3,10 +3,33 @@ require "./spec_helper"
 class TestClass
 end
 
+module TestModule
+  extend self
+
+  def test_function
+  end
+
+  CONSTANT = 1
+
+  class TClass
+  end
+
+  module SubModule
+  end
+end
+
+include_module(TestModule)
+
 describe "include_module" do
   it "raises error when argument is not a module" do
     expect_raises Exception, "TestClass is not a module" do
       include_module TestClass
     end
+  end
+
+  it "includes constants, classes and methods" do
+    TClass.should eq TestModule::TClass
+    CONSTANT.should eq TestModule::CONSTANT
+    test_function.should eq TestModule.test_function
   end
 end
--- src/include.cr
+++ src/include.cr
@@ -1,6 +1,14 @@
 macro include_module(module_name)
-  {% if module_name.resolve.module? %}
-  {% else %}
+  {% resolved_module = module_name.resolve %}
+  {% unless resolved_module.module? %}
     raise "{{module_name.id}} is not a module"
+  {% end %}
+  {% for method in resolved_module.methods %}
+    def {{ method.name }}({{ *method.args }})
+      {{ method.body }}
+    end
+  {% end %}
+  {% for constant in resolved_module.constants %}
+    {{ constant.id }} = {{ resolved_module.name(generic_args: false) }}::{{ constant.id }}
   {% end %}
 end
